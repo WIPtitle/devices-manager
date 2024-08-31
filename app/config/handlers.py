f21fec1170e3d6da406f03469e90cb8e2eda15cf
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-08-31 22:29:03 +0200

    fix: fix imports

--- app/config/handlers.py
+++ app/config/handlers.py
@@ -1,7 +1,8 @@
 from fastapi.responses import JSONResponse
 from fastapi import Request
 
-from app.exceptions import NotFoundException, BadRequestException
+from app.exceptions.BadRequestException import BadRequestException
+from app.exceptions.NotFoundException import NotFoundException
 
 
 async def not_found_exception_handler(request: Request, exc: NotFoundException):
