5a6371e8dce82028517579154d42f9c963a65575
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-08-31 11:45:57 +0200

    fix: removed useless package

--- app/utils/modules_discover.py
+++
@@ -1,8 +0,0 @@
-import pkgutil
-
-def discover_all_modules(package):
-    modules = []
-    for _, name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + "."):
-        if not is_pkg:
-            modules.append(name)
-    return modules
