1db203af43d831681cb029c6b95dee0e9da6e50e
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-08-31 13:14:59 +0200

    feat: docker compose now works

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from app.routers.impl.gpio_config_router import GpioConfigRouter
 from app.routers.router_wrapper import RouterWrapper
 
+
 routers: List[RouterWrapper] = [
     GpioConfigRouter()
 ]
