8ec5064cd0b8fbbc68d06b3e85e7601a96e75e33
galvagnimatteo <galvagni.matteo@protonmail.com>
2026-02-22 11:37:26 +0100

    fix: better caching

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 
-cd /app
+mkdir -p /var/lib/devices-manager/data/alarm_recordings
+mkdir -p /var/lib/devices-manager/data/recordings
 
-pip install --upgrade pip
-
-apt-get update && apt-get install -y libpq-dev git ffmpeg libsm6 libxext6
-pip install --no-cache-dir -r requirements.txt
-
-# create directories for recordings
-mkdir /var/lib/devices-manager/data/alarm_recordings
-mkdir /var/lib/devices-manager/data/recordings
-
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
-# Loop until server up to force initialization
-while true; do
-    response=$(curl --write-out "%{http_code}" --silent --output /dev/null http://0.0.0.0:8000)
-    if [ "$response" -eq 404 ]; then
-        break
-    else
-        sleep 1
-    fi
-done
-
-wait
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
